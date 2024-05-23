ade56940161cedef9ed17f9a207be970cbf0e2c4
Gabriel Alves <gabriel777alves00@gmail.com>
2024-05-23 17:44:20 -0300

    implements store and update repository support

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
+use App\Services\SupportService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
+
+class SupportController extends Controller
+{
+    public function __construct(
+        protected SupportService $service
+    ) {}
+
+    public function index(Request $request)
+    {
+        $supports = $this->service->getAll($request->filter);
+        dd($supports);
+        return view('admin.supports.index', compact('supports'));
+    }
+
+    public function create()
+    {
+        return view('admin.supports.create');
+    }
+
+    public function store(StoreUpdateSupport $request)
+    {
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
+        );
+
+        return redirect()->route('supports.index');
+    }
+
+    public function show(string $id)
+    {
+        if(!$support = $this->service->findOne($id)) {
+            return redirect()->back();
+        }
+
+        return view('admin.supports.show', compact('support'));
+
+    }
+
+    public function edit(string $id)
+    {
+        //if(!$support = $support->where('id', '=', $id)->first()) {
+        if(!$support = $this->service->findOne($id)) {
+            return redirect()->back();
+        }
+
+        return view('admin.supports.edit', compact('support'));
+    }
+
+    public function update(StoreUpdateSupport $request, string $id)
+    {
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request)
+        );
+
+        if(!$support) {
+            return redirect()->back();
+        }
+
+        return redirect()->route('supports.index');
+
+    }
+
+    public function destroy(string $id)
+    {
+        $this->service->delete($id);
+
+        return redirect()->route('supports.index');
+    }
+}
---
+++ app/Http/Requests/StoreUpdateSupport.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateSupport extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'subject' => [
+                'required',
+                'min:3',
+                'max:255',
+                'unique:supports'
+            ],
+            'body' => [
+                'required',
+                'min:3',
+                'max:10000'
+            ]
+        ];
+
+        if($this->method() === 'PUT') {
+            $rules['subject'] = [
+                'required',
+                'min:3',
+                'max:255',
+                // há uma excessão quando o $id enviado para a rota
+                // é igual ao id da coluna "id"
+                // "unique:supports,subject,{$this->id},id"
+                Rule::unique('supports')->ignore($this->id)
+            ];
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            // subject
+            'subject.required' => 'O assunto é obrigatório',
+            'subject.min' => 'O assunto deve ter 3 ou mais caracteres',
+            'subject.max' => 'O assunto deve ter no máximo 255 caracteres',
+            'subject.unique' => 'O assunto já existe',
+            // body
+            'body.required' => 'A descrição é obrigatória',
+            'body.min' => 'A descrição deve ter 3 ou mais caracteres',
+            'body.max' => 'A descrição deve ter no máximo 255 caracteres',
+        ];
+    }
+}
