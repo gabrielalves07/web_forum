b9986054573991e27ba5b889ba5f6f4f0a6d55b3
Gabriel Alves <gabriel777alves00@gmail.com>
2024-06-06 12:31:28 -0300

    styling show supports view

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -1,13 +1,22 @@
-<h1>Detalhes: dúvida {{ $support->id }}</h1>
+@extends('template.layout')
 
-<ul>
-    <li>Assunto: {{ $support->subject }}</li>
-    <li>Descrição: {{ $support->body }}</li>
-    <li>Status: {{ $support->status }}</li>
-</ul>
+@section('content')
 
-<form action="{{ route('supports.destroy', $support->id) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Excluir</button>
-</form>
+    <div class="container">
+        <h1 class="my-4">Detalhes: dúvida {{ $support->id }}</h1>
+        <ul>
+            <li>Assunto: {{ $support->subject }}</li>
+            <li>Descrição: {{ $support->body }}</li>
+            <li>Status: {{ $support->status }}</li>
+        </ul>
+        <form action="{{ route('supports.destroy', $support->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <div class="buttons-footer d-flex gap-2">
+                <a href="{{ route('supports.index') }}" class="btn btn-outline-secondary">Voltar</a>
+                <button type="submit" class="btn btn-danger">Excluir</button>
+            </div>
+        </form>
+    </div>
+
+@endsection
